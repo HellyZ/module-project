9ff1cfd3357ad06c58bcb8e1f609ad2daf118e9c
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-01 16:14:47 +0200

    fix blur menu click

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,13 @@
 const menu = () => {
   const menu = document.querySelector("menu");
-  const menuItems = document.querySelectorAll("menu ul>li>a");
-
-  const handleMenu = () => {
-    menu.classList.toggle("active-menu");
-  };
 
   document.addEventListener("click", (e) => {
     if (e.target.closest(".menu")) {
-      handleMenu();
+      menu.classList.toggle("active-menu");
     }
-    if (
+    else if (
       e.target.classList.contains("close-btn") ||
-      e.target.matches("menu ul>li>a")
+      e.target.matches("menu ul>li>a") || !e.target.closest("menu")
     ) {
       menu.classList.remove("active-menu");
     }

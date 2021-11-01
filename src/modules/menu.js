e10fd5d97550d3cae28d55951769b109ce7b83ad
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-01 16:08:11 +0200

    clearfix

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,12 @@
 const menu = () => {
   const menu = document.querySelector("menu");
   const menuItems = document.querySelectorAll("menu ul>li>a");
-  console.log(menuItems);
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
   };
 
   document.addEventListener("click", (e) => {
-    console.log(e.target);
     if (e.target.closest(".menu")) {
       handleMenu();
     }

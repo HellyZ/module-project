8aa42e4484ea0838fa3e75dc72f2990b76fe17d8
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-03 19:37:04 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import handleValidation from "./modules/validateFields";
 import sliderSwiper from "./modules/sliderSwiper";
 import Tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer("04 november 2021");
 menu();
@@ -14,3 +15,4 @@ sliderSwiper();
 handleValidation();
 Tabs();
 slider();
+calc();
